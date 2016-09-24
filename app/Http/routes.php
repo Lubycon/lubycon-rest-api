8047225b429361f1d0c01e7c228504c8ad7a05e2
daniel_zepp poterble <s_u_n_e@naver.com>
2016-09-24 17:02:41 +0900

    correct controller bugs

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,7 +1,7 @@
 <?php
 
 Route::post('members/signin', 'Auth\AuthController@signin');
-Route::get('members/signout', 'Auth\AuthController@signout');
+Route::put('members/signout', 'Auth\AuthController@signout');
 Route::post('members/signup', 'Auth\AuthController@signup');
 
 Route::get('members/signdrop',function(){
