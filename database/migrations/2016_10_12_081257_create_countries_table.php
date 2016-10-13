cabd881dc6393a4a4bf9ae9c59092c92c7accba4
daniel_zepp poterble <s_u_n_e@naver.com>
2016-10-13 20:11:01 +0900

    country seed make

--- database/migrations/2016_10_12_081257_create_countries_table.php
+++ database/migrations/2016_10_12_081257_create_countries_table.php
@@ -13,9 +13,37 @@ class CreateCountriesTable extends Migration
     public function up()
     {
         Schema::create('countries', function (Blueprint $table) {
-            $table->increments('country_id');
+            $table->integer('country_id')->unsigned()->index();
             $table->tinyInteger('utc');
-            $table->enum('region',['east asia', 'central asia', 'south asia', 'northern asia', 'south east asia', 'south west asia', 'eastern europe', 'central europe', 'western europe', 'southern europe', 'northern europe', 'south east europe','south west europe', 'eastern africa', 'central africa', 'western africa','northern africa','southern africa']);
+            $table->enum('region',[
+                'east asia',
+                'central asia',
+                'south asia',
+                'south west asia',
+                'northern asia',
+                'south east asia',
+                'south west asia',
+                'eastern europe',
+                'central europe',
+                'western europe',
+                'southern europe',
+                'northern europe',
+                'south east europe',
+                'south west europe',
+                'eastern africa',
+                'central africa',
+                'western africa',
+                'northern africa',
+                'southern africa',
+                'pacific',
+                'west indies',
+                'indian ocean',
+                'undecided',
+                'north america',
+                'northern america',
+                'south america',
+                'central america',
+            ]);
             $table->enum('continent',['asia', 'europe', 'africa', 'americas', 'oceania','undecided']);
             $table->string('name',255);
             $table->timestamps();
