7e6e2af74473975bce5d31a71e1a74d384c497cf
daniel_zepp <bboyzepot@gmail.com>
2016-11-26 15:49:03 +0900

    error config make

---
+++ config/error.php
@@ -0,0 +1,16 @@
+<?php
+
+// return [
+//     'custom' => [
+//         'App'       => Illuminate\Support\Facades\App::class,
+//     ]
+// ];
+
+return [
+    "0010" => (object)array(
+        "httpCode" => 403,
+        "customCode" => "0010",
+        "msg" => "Login Failure",
+        "devMsg" => "not good email, password"
+    )
+];
